2bcffd8d3497a5b0d1ce514f85a570cd69cef974
syeda <syedazhartalha@gmail.com>
2018-07-13 20:50:33 +0530

    making frontend mobile compatible

--- frontend/src/header/index.js
+++ frontend/src/header/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Redirect, Link} from "react-router-dom";
 class MMHeader extends Component{
+    
     constructor(props)
     {
         super(props);
@@ -49,12 +50,91 @@ class MMHeader extends Component{
     render(){
         const {title} = this.props;
         const isLoggedIn = this.props.isLoggedIn;
+        var link_style = {
+            margin: "8px",
+            color: "white"
+        }
+        var search_div = {
+            width: "1000px"
+        }
+        
+        var search_bar = {
+            width: "200px",
+            marginRight: "10px"
+        }
+
+        var search_type = {
+            width: "90px",
+            marginRight: "10px"
+        }
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="header">
                 <Link to="/moviemania/"><h1 className="Title">{title}</h1></Link>
+                <div className="header-center" style={search_div}>
+                    <div className="form-inline">
+                        <input style={search_bar} className="form-control" type="text" id="search-bar" onChange={()=> this.updateSearchValue()} placeholder="Search"/>
+                        <select style={search_type} multiple="" className="form-control" onChange={() => this.updateSearchType()} id="search-select">
+                            <option value="movie">movie</option>
+                            <option value="celeb">celeb</option>
+                        </select>
+                        <Link className="btn btn-secondary my-2 my-sm-0" to={
+                            this.state.searchURL + this.state.searchType +"&value=" + this.state.searchValue
+                            }>
+                            Search
+                        </Link>
+                    </div>
+                    <div className="header-center-bottom">
+                        <span>
+                                <Link style={link_style} to="/moviemania/">Home</Link>
+                        </span>
+                        <span>
+                                <Link style={link_style} to="/moviemania/movies">Movies</Link>
+                        </span>
+                        <span>
+                                <Link style={link_style} to="/moviemania/celebs">Celebs</Link>
+                        </span>
+                        <span>
+                                <Link style={link_style} to="/moviemania/genres">Genres</Link>
+                        </span>
+                    </div>
+                </div>
+                <div className="header-right">
+                    {
+                        isLoggedIn?
+                        <ul className="nav nav-pills">
+                            <li className="nav-item dropdown show" id="MMdropdown">
+                                <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="javascript:void(0);" role="button" aria-haspopup="true" aria-expanded="false" id="MMdropbtn">{this.props.username}</a>
+                                    <div x-placement="bottom-start" id="MMdropdown-content">
+                                        <Link to="/moviemania/account">Account</Link>
+                                        <a href="javascript:void(0);" onClick={this.logout}>Logout</a>
+                                    </div>
+                            </li>
+                        </ul>:
+                        <div>
+                            <span>
+                                <Link style={link_style} to ='/moviemania/login'>Login</Link>
+                            </span>
+                            <span>
+                                <Link style={link_style} to ='/moviemania/signup'>SignUp</Link>
+                            </span>
+                        </div>
+                    }
+                </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default MMHeader;
+
+
+/*<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div className="header">
+                <Link to="/moviemania/"><h1 className="Title">{title}</h1></Link>
                 <div className="header-center">
-                    <div className="form-inline my-2 my-lg-0">
+                    <div className="form-inline">
                         <input className="form-control mr-sm-2" type="text" id="search-bar" onChange={()=> this.updateSearchValue()} placeholder="Search"/>
                         <select multiple="" className="form-control" onChange={() => this.updateSearchType()} id="search-select">
                             <option value="movie">movie</option>
@@ -106,9 +186,4 @@ class MMHeader extends Component{
                     }
                 </div>
                 </div>
-            </nav>
-        );
-    }
-}
-
-export default MMHeader;
+            </nav>*/
